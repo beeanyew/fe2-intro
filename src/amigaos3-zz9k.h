dc9f6b4ad53da4750bb3c414122bc27fadbb6a09
Bjorn Astrom <beeanyew@gmail.com>
2020-08-02 03:54:02 +0200

    Remove ScummVM source header stubs
    I moved these files over from the work I'd been doing on the AmigaOS 3 port of ScummVM, here:
    https://github.com/beeanyew/scummvm-amigaos3/tree/zz9k-stuff
    Pretty sure they don't actually need to be in these files. :)

--- src/amigaos3-zz9k.h
+++ src/amigaos3-zz9k.h
@@ -1,28 +1,3 @@
-/* ScummVM - Graphic Adventure Engine
- *
- * ScummVM is the legal property of its developers, whose names
- * are too numerous to list here. Please refer to the COPYRIGHT
- * file distributed with this source distribution.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
-
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
-
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- *
- * $URL$
- * $Id$
- *
- */
-
 // A pointer to the permanent GFXData struct in memory on the ZZ9000 board,
 // used for sharing data between the Amiga side and the RTG board.
 #define Z3_GFXDATA_ADDR 0x3200000
